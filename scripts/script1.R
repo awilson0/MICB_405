d946853a48b4f9ae3e4fe52a0e0b0e3d2a81bcd1
JJ <jiinhum@gmail.com>
2019-11-30 19:29:52 -0800

    Added scripts for taxonomy vs RPKM plots and associated KEGG/gene name data

--- scripts/script1.R
+++ scripts/script1.R
@@ -4,13 +4,16 @@
 library(tidyverse)
 library(pathview)
 library(cowplot)
+library(KEGGREST)
 
-setwd("~/micb_405/MICB_405/")
+asetwd("~/micb_405/MICB_405/")
 checkm <- read_tsv("data/MetaBAT2_SaanichInlet_10m_min1500_checkM_stdout.tsv")
 arc <- read_tsv("data/gtdbtk.ar122.classification_pplacer.tsv", col_names = FALSE)
 bac <- read_tsv("data/gtdbtk.bac120.classification_pplacer.tsv", col_names = FALSE)
-rpkm <- read_csv("data/SaanichInlet_10m_binned.rpkm.csv")
-rpkm2 <- read_csv("data/SI072_10m_MAG_RPKM.csv")
+rpkm_g <- read_csv("data/SaanichInlet_10m_binned.rpkm.csv")
+rpkm_t <- read_csv("data/SI072_SaanichInlet_MAG_ORFs.sam_RPKM.csv", col_names = FALSE)
+prk <- read_csv("data/Prokka_MAG_map.csv", col_names = FALSE)
+ko <- read_tsv("data/SaanichInlet_MAGx_ORFs_ko.cleaned.txt", col_names = FALSE)
 
 # Separate out taxonomy info, combine into 1 dataframe
 a1 <- arc$X2
@@ -32,7 +35,7 @@ tax <- bind_rows(arc1, bac1) %>%
   mutate_if(is_character, funs(na_if(.,"")))
 
 # Sum rpkms and add column matching `Bin id`
-r2 <- rpkm2 %>% separate(Sequence_name, into = c("Bin", "num"), sep="(?<=SaanichInlet_10m_...)") %>% 
+r2 <- rpkm_g %>% separate(Sequence_name, into = c("Bin", "num"), sep="(?<=SaanichInlet_10m_...)") %>% 
   group_by(Bin) %>% 
   summarize(RPKM =  sum(RPKM)) %>% 
   mutate(Bin = gsub('m_', 'm.', Bin)) %>% 
@@ -64,18 +67,21 @@ dat$Phylum <- factor(dat$Phylum, levels = c("Actinobacteriota", "Bacteroidota", 
 # Assign nice colours to each phylum
 phylum_colours <- c("#9c47d5","#a75e3d","#697d36","#c08426","#725ec2","#d6482e","#6778b6","#be4964","#5ea536","#b44d95","#3c9472")
 
+color1 <- c("#00cf64","#e60096","#9688ff","#ff8f0c","#7ad0ff","#a46c00","#46044d","#76d9a8","#650017","#ffab7b","#ff949b")
+
 # Plot Contamination vs Completion
 contamination_plot <- ggplot(dat, aes(x = Completeness, y = Contamination)) +
   geom_point(aes(colour = Phylum, size = RPKM, shape = Domain))+
   scale_shape_manual(values = c(17, 16)) + 
-  scale_color_manual(values = phylum_colours)+
+  scale_colour_manual(values = color1) +
   geom_hline(yintercept = 0) + geom_hline(yintercept = 1.25) +
   geom_vline(xintercept = 90) + geom_vline(xintercept = 100)+
   geom_segment(aes(x=90,y=1.25,xend=100,yend=1.25), color = "blue") +
   geom_segment(aes(x=90,y=0,xend=90,yend=1.25), color = "blue")+
   geom_segment(aes(x=100, y=0, xend = 100, yend = 1.25), color = "blue") +
-  geom_segment(aes(x = 90, y=0, xend = 100, yend = 0), color = "blue")+
-  theme_cowplot()
+  geom_segment(aes(x = 90, y=0, xend = 100, yend = 0), color = "blue") +
+  theme_cowplot() +
+  guides(color = guide_legend(override.aes = list(size = 4)))
 
 contamination_plot
 
@@ -85,5 +91,94 @@ dat1 <- dat %>% filter(Completeness > 90) %>% filter(Contamination < 10)
 zoomplot <- ggplot(dat1, aes(x = Completeness, y = Contamination)) +
   geom_point(aes(colour = Phylum, size = RPKM))+
   scale_shape_manual(values = c(17, 16)) + 
-  theme_cowplot()
+  scale_colour_manual(values = color1) +
+  theme_cowplot() +
+  guides(color = guide_legend(override.aes = list(size = 4)))
 zoomplot
+
+# Counting total MAGs with RPKM + taxonomy data
+dat2 <- dat %>% filter(!is.na(RPKM))
+table(dat1$Phylum)
+table(dat1$Class)
+
+## Prokka data wrangling
+ko1 <- ko %>% dplyr::rename("ORF" = "X1", "Kegg_ID" = "X2")
+prk1 <- prk %>% mutate(X2 = gsub('m_', 'm.', X2)) %>% dplyr::rename("Prokka_Id" = "X1", `Bin Id` = "X2")
+rpkm_t2 <- rpkm_t %>% dplyr::rename("ORF" = "X1", "RPKM" = "X2")
+
+# Join all data frames
+all <- left_join(ko1, rpkm_t2, by = "ORF") %>% 
+  separate(ORF, into = c("Prokka_Id", "ORF")) %>% 
+  left_join(prk1, by = "Prokka_Id") %>% 
+  left_join(tax, by = "Bin Id") %>% 
+  left_join(checkm, by = "Bin Id") %>% 
+  select(-"Marker lineage", -"# genomes", -"# markers", -"# marker sets", 
+         -"0", -"1", -"2", -"3", -"4", -"5+", -"Strain heterogeneity")
+
+# Get KEGGREST lookups and reformat into tables with proper headings
+zyms <-keggLink("enzyme","ko")
+zym1 <- keggList("enzyme")
+paths <-keggLink("enzyme", "pathway")
+
+enzymes <- enframe(zyms) %>% 
+  rename("Kegg_ID" = "name", "num" = "value") %>% 
+  mutate(Kegg_ID = gsub('ko:K', 'K', Kegg_ID)) 
+zym2 <- enframe(zym1) %>% 
+  rename("num" = "name", "Enzyme" = "value") %>% 
+  separate(Enzyme, sep = ";", into = c("Enzyme", "Enzyme2"), extra = "merge")
+pathways <- enframe(paths) %>% rename("Path" = "name", "num" = "value")
+
+# Get gene names by uploading KO list to KEGG mapper
+# Copy pasted list of genes "names.csv"
+names <- read_csv("data/names.csv", col_names = FALSE) %>% 
+  mutate(X1 = gsub("ko:", "", X1)) %>% 
+  separate(X1, into = c("Kegg_ID", "Gene"), sep = "(?<=K.....)") %>% 
+  separate(Gene, into = c("Gene", "Enzyme2"), sep = ";", extra = "merge")
+
+# Combine tables
+alldat <- left_join(enzymes, zym2, by = "num") %>% 
+  left_join(pathways, by = "num") %>% 
+  left_join(all, by = "Kegg_ID") %>% 
+  left_join(names, by = "Kegg_ID") %>% 
+  filter(RPKM != 0)
+
+# Most abundant pathways?
+toppath <- alldat %>% 
+  group_by(Path) %>% 
+  summarize(RPKM =  sum(RPKM)) %>% 
+  arrange(desc(RPKM)) 
+
+# Looking at C fixation pathway
+cfix <- alldat %>% filter(Path == "path:map00720")
+
+cfplot <- 
+  ggplot(cfix, aes(y = Gene, x = Class, color = Phylum)) +
+  geom_point(aes(size = RPKM)) +
+  theme_cowplot() +
+  theme(axis.text.x = element_text(angle = 20, hjust = 1),
+        panel.grid.major.y = element_line(color = "gray")) +
+  guides(color = guide_legend(override.aes = list(size = 4)))
+cfplot
+  
+# Looking at oxidative phosphorylation pathway
+oxph <- alldat %>% filter(Path == "path:map00190")
+
+oxplot2 <- 
+  ggplot(oxph, aes(y = Gene, x = Class, color = Phylum)) +
+  geom_point(aes(size = RPKM)) +
+  theme_cowplot() +
+  theme(axis.text.x = element_text(angle = 20, hjust = 1),
+        panel.grid.major.y = element_line(color = "gray")) +
+  guides(color = guide_legend(override.aes = list(size = 4)))
+oxplot2
+
+# Top 10 oxphos genes
+oxkeggs <- arrange(oxph, desc(RPKM)) %>%
+  distinct(Kegg_ID, .keep_all = TRUE) %>% slice(1:10)
+
+oxbac <- filter(oxph, Class == "Bacteroidia") %>% 
+  distinct(Kegg_ID, .keep_all = TRUE) %>% 
+  arrange(desc(RPKM))
+
+
+
