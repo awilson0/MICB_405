1c1961ce42d2f4e1265fa40afb73524e2596562c
aggiesslo <33464613+aggiesslo@users.noreply.github.com>
2019-12-02 05:27:22 -0800

    Added updated bwa and prokka code

---
+++ scripts/project2_bwa.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+
+mkdir /home/alo_mb19/BWA_output
+out_dir='/home/alo_mb19/BWA_output'
+MetaT_reads_dir='/projects/micb405/resources/project_2/2019/SaanichInlet_10m/MetaT_reads'
+
+
+#make index
+bwa index -p $out_dir/BWA_index /home/alo_mb19/SaanichInlet_MAG_ORFs.ffn #change fna->ffn
+echo "Done making index"
+
+array=($MetaT_reads_dir/*)
+
+
+#align reads and create SAM files for every cruise at 10m
+for f in "${array[@]}";
+do
+	#name=${f##*/} #retain part after last /
+	bwa mem -t 8 -p $out_dir/BWA_index $f > $out_dir/SI072_SaanichInlet_MAG_ORFs.sam 
+done
+
+echo "Done creating SAM files"
